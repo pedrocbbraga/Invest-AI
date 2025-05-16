458733ccdbbc3d2ce4d0f7898f59c82f8cda4ab5
Shiv <sac468@drexel.edu>
2025-05-15 22:43:48 -0400

    F&G small fix

--- environment/run_env_test.py
+++ environment/run_env_test.py
@@ -7,7 +7,7 @@ obs, _ = env.reset()
 done = False
 step = 0
 
-while not done and step < 10:  # test first 10 steps
-    action = [0.5]  # neutral allocation (50%)
+while not done and step < 10:  
+    action = [0.5]  
     obs, reward, done, _, _ = env.step(action)
     step += 1
